6485816b5a0d7e3ab4114cc1c12b658f90cce799
Adrien Pestel <pestouille@gmail.com>
2020-12-02 22:29:38 +0100

    change create_spk script include nomad release download

--- create_spk.sh
+++ create_spk.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-
-cd 1_create_package 
+set +x
+NOMAD_VERSION=0.12.9
+NOMAD_ZIP=nomad_${NOMAD_VERSION}_linux_amd64.zip
+cd 1_create_package/nomad 
+wget https://releases.hashicorp.com/nomad/${NOMAD_VERSION}/${NOMAD_ZIP}
+unzip ${NOMAD_ZIP}
+rm ${NOMAD_ZIP}
+cd ..
 tar cvfz package.tgz * 
 mv package.tgz ../2_create_project/ 
 cd ../2_create_project/ 
